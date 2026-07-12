12dcc077f003c36d5800b97ad5f1524d949cae72
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "traffic_manager_profiles" {
-  description = "All traffic_manager_profile resources"
-  value       = azurerm_traffic_manager_profile.traffic_manager_profiles
-}
 output "traffic_manager_profiles_dns_config" {
-  description = "List of dns_config values across all traffic_manager_profiles"
-  value       = [for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : v.dns_config]
+  description = "Map of dns_config values across all traffic_manager_profiles, keyed the same as var.traffic_manager_profiles"
+  value       = { for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : k => v.dns_config }
 }
 output "traffic_manager_profiles_fqdn" {
-  description = "List of fqdn values across all traffic_manager_profiles"
-  value       = [for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : v.fqdn]
+  description = "Map of fqdn values across all traffic_manager_profiles, keyed the same as var.traffic_manager_profiles"
+  value       = { for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : k => v.fqdn }
 }
 output "traffic_manager_profiles_max_return" {
-  description = "List of max_return values across all traffic_manager_profiles"
-  value       = [for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : v.max_return]
+  description = "Map of max_return values across all traffic_manager_profiles, keyed the same as var.traffic_manager_profiles"
+  value       = { for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : k => v.max_return }
 }
 output "traffic_manager_profiles_monitor_config" {
-  description = "List of monitor_config values across all traffic_manager_profiles"
-  value       = [for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : v.monitor_config]
+  description = "Map of monitor_config values across all traffic_manager_profiles, keyed the same as var.traffic_manager_profiles"
+  value       = { for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : k => v.monitor_config }
 }
 output "traffic_manager_profiles_name" {
-  description = "List of name values across all traffic_manager_profiles"
-  value       = [for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : v.name]
+  description = "Map of name values across all traffic_manager_profiles, keyed the same as var.traffic_manager_profiles"
+  value       = { for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : k => v.name }
 }
 output "traffic_manager_profiles_profile_status" {
-  description = "List of profile_status values across all traffic_manager_profiles"
-  value       = [for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : v.profile_status]
+  description = "Map of profile_status values across all traffic_manager_profiles, keyed the same as var.traffic_manager_profiles"
+  value       = { for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : k => v.profile_status }
 }
 output "traffic_manager_profiles_resource_group_name" {
-  description = "List of resource_group_name values across all traffic_manager_profiles"
-  value       = [for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : v.resource_group_name]
+  description = "Map of resource_group_name values across all traffic_manager_profiles, keyed the same as var.traffic_manager_profiles"
+  value       = { for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : k => v.resource_group_name }
 }
 output "traffic_manager_profiles_tags" {
-  description = "List of tags values across all traffic_manager_profiles"
-  value       = [for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : v.tags]
+  description = "Map of tags values across all traffic_manager_profiles, keyed the same as var.traffic_manager_profiles"
+  value       = { for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : k => v.tags }
 }
 output "traffic_manager_profiles_traffic_routing_method" {
-  description = "List of traffic_routing_method values across all traffic_manager_profiles"
-  value       = [for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : v.traffic_routing_method]
+  description = "Map of traffic_routing_method values across all traffic_manager_profiles, keyed the same as var.traffic_manager_profiles"
+  value       = { for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : k => v.traffic_routing_method }
 }
 output "traffic_manager_profiles_traffic_view_enabled" {
-  description = "List of traffic_view_enabled values across all traffic_manager_profiles"
-  value       = [for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : v.traffic_view_enabled]
+  description = "Map of traffic_view_enabled values across all traffic_manager_profiles, keyed the same as var.traffic_manager_profiles"
+  value       = { for k, v in azurerm_traffic_manager_profile.traffic_manager_profiles : k => v.traffic_view_enabled }
 }
 
